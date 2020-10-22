25ca34fbde6ad3be77199e638f2afa26b3b00bf5
Sebastian-Perz <esdo.perz@gmail.com>
2020-10-22 18:01:57 +0200

    Updates

---
+++ precache-manifest.f926bd2b3e46b5b8600f6aae8da811ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abde13f841f9526e1813e2e03648c62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcbf3ceb0681378df287",
+    "url": "/static/css/main.73285b7b.chunk.css"
+  },
+  {
+    "revision": "de7f0eb7fc1f3bff4de2",
+    "url": "/static/js/2.23ddacb4.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.23ddacb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbf3ceb0681378df287",
+    "url": "/static/js/main.6c571444.chunk.js"
+  },
+  {
+    "revision": "affdc8648b31ecc31c18",
+    "url": "/static/js/runtime-main.25af6fb3.js"
+  },
+  {
+    "revision": "be572c126a54a1703f34378e38b55ab2",
+    "url": "/static/media/cold.be572c12.jpg"
+  },
+  {
+    "revision": "f6ffbe9430766cb596f3ec6b35eb21a4",
+    "url": "/static/media/sunny.f6ffbe94.jpg"
+  }
+]);
